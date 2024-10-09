0cad0e97f968605311afc8c4ba558971686a9da1
Ephantus Mwangi <62132661+the-nulldev@users.noreply.github.com>
2024-10-09 13:46:48 +0300

    feat: new function upper

---
+++ src/CaseOperations.java
@@ -0,0 +1,8 @@
+public class CaseOperations {
+
+    // Method to convert text to uppercase
+    public static void makeUpper(String text) {
+        System.out.println(text.toUpperCase());
+    }
+}
+
